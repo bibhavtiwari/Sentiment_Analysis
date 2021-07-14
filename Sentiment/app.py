718965fa257db9c2abb5329b788d7978b1f36192
bibhav <bibhavtiwari@gmail.com>
2021-07-14 15:09:15 +0530

    project

---
+++ Sentiment/app.py
@@ -0,0 +1,41 @@
+# -- coding: utf-8 --
+
+import numpy as np
+import pickle
+from flask import Flask, request, render_template
+
+# Load ML model
+model = pickle.load(open('model.pkl', 'rb')) 
+
+# Create application
+app = Flask(__name__)
+
+# Bind home function to URL
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# Bind predict function to URL
+@app.route('/predict', methods =['POST'])
+def predict():
+    
+    # Put all form entries values in a list 
+    features = [float(i) for i in request.form.values()]
+    # Convert features to array
+    array_features = [np.array(features)]
+    # Predict features
+    prediction = model.predict(array_features)
+    
+    output = prediction
+    
+    # Check the output values and retrive the result with html tag based on the value
+    if output == ['negative']:
+        return render_template('negative.html', 
+                               result = 'The patient is not likely to have heart disease!')
+    else:
+        return render_template('positive.html', 
+                               result = 'The patient is likely to have heart disease!')
+
+if __name__ == '_main_':
+#Run the application
+    app.run()
